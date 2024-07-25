75837c0fcc396b4ffe47e3349796768d40649bfc
lingmo-dream <2422586862@qq.com>
2024-07-25 09:39:06 +0800

    [update]lpm

--- src/lpm/lpm.c
+++ src/lpm/lpm.c
@@ -242,12 +242,13 @@ static void usage(int op, const char * const myname)
 static void version(void)
 {
 	printf("\n");
-	printf(" .--.                  Pacman v%s - libalpm v%s\n", PACKAGE_VERSION, alpm_version());
-	printf("/ _.-' .-.  .-.  .-.   Copyright (C) 2006-2024 Pacman Development Team\n");
-	printf("\\  '-. '-'  '-'  '-'   Copyright (C) 2002-2006 Judd Vinet\n");
-	printf(" '--'\n");
+	printf(" .--.                  Lingmo OS Package Manager v%s - libalpm v%s\n", PACKAGE_VERSION, alpm_version());
+	printf("/ _.-' .-.  .-.  .-.   Copyright (C) 2024 Lingmo OS Team\n");
+	printf("\\  '-. '-'  '-'  '-'  Copyright (C) 2006-2024 Pacman Development Team\n");
+	printf(" '--'                  Copyright (C) 2002-2006 Judd Vinet\n");
 	printf(_("                       This program may be freely redistributed under\n"
 	         "                       the terms of the GNU General Public License.\n"));
+	printf("Thanks for Pacman project and Arch Linux Team.\n");
 	printf("\n");
 }
 
